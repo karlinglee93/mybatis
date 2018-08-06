78d426db2b4bea4bc5e876dd3940f7675305666e
karlinglee93 <459607025@qq.com>
2018-08-06 15:27:51 +0800

    Mybatis - one to mary

--- src/com/safari/pojo/Category.java
+++ src/com/safari/pojo/Category.java
@@ -1,8 +1,12 @@
 package com.safari.pojo;
 
+import java.util.List;
+
 public class Category {
 	private int id;
 	private String name;
+	// 修改Category实体类，提供products的集合
+	List<Product> products;
 
 	public int getId() {
 		return id;
@@ -18,5 +22,18 @@ public class Category {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
 	}
 }
