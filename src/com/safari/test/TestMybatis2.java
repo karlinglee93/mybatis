588e3250317d65621efee9f169c1faf6b13ccc00
karlinglee93 <459607025@qq.com>
2018-08-06 16:01:19 +0800

    mary to one

---
+++ src/com/safari/test/TestMybatis2.java
@@ -0,0 +1,31 @@
+package com.safari.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+ 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+ 
+import com.safari.pojo.Product;
+  
+public class TestMybatis2 {
+  
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+  
+        List<Product> ps = session.selectList("listProduct");
+        for (Product p : ps) {
+            System.out.println(p+" 对应的分类是 \t "+ p.getCategory());
+        }
+ 
+        session.commit();
+        session.close();
+  
+    }
+}
